6b9bf6f0ecbcb6dd9f99d37242ba2533db872031
Hiroaki <wand1250@gmail.com>
2017-03-14 13:29:38 +0900

    スートを4種類に / プレイヤーを10人に / ストレートのランクの比較を修正 / コンソール出力を調整 / コメント付与

--- js/card.js
+++ js/card.js
@@ -6,7 +6,6 @@ const SuitType = {
   Heart: 1,
   Diamond: 2,
   Club: 3,
-  Sardine: 4
 } 
 
 const RankName = {
@@ -25,13 +24,20 @@ const RankName = {
   13: 'K'
 }
 
+const SuitName = {};
+SuitName[SuitType.Spade] = 'Sp';
+SuitName[SuitType.Heart] = 'He';
+SuitName[SuitType.Diamond] = 'Di';
+SuitName[SuitType.Club] = 'Cl';
+
 const MinimumRank = 1;
 const SuitCount = Object.keys(SuitType).length;
 const RankCount = Object.keys(RankName).length;
 const HandCardCount = 5;
 const CommunityCardCount = 5;
 const HoldsCardCount = 2;
-const PlayerCount  = 30;
+const PlayerCount  = 10;
+// 役の名前と条件
 const HandConditions = [
 {
   name: 'High Card',
@@ -75,55 +81,67 @@ const HandConditions = [
 }
 ];
 
+// 役判定ロジック
 class HandChecker {
   constructor(hand) {
     this.hand = hand;
     this.hand.sort((a,b) => a.pokerRank() - b.pokerRank());
 
-    // ランクごとのカード枚数
-    this.rankNums = [];
-    for(let i = 0;i <= RankCount + 1; ++i) {
-      this.rankNums[i] = 0;
-    }
-    this.hand.forEach(card => ++this.rankNums[card.pokerRank()]);
-    // n枚のペアの数
-    this.groupCounts = [];
-    this.groupRanks = [];
-    for(let i = 0;i <= HandCardCount; ++i) {
-      this.groupCounts[i] = 0;
-      this.groupRanks[i] = [];
-    }
+    // ペア系判定
+    {
+      // ランクごとのカード枚数
+      this.rankNums = [];
+      for(let i = 0;i <= RankCount + 1; ++i) {
+        this.rankNums[i] = 0;
+      }
+      this.hand.forEach(card => ++this.rankNums[card.pokerRank()]);
+      // n枚のペアの数
+      this.groupCounts = [];
+      this.groupRanks = [];
+      for(let i = 0;i <= HandCardCount; ++i) {
+        this.groupCounts[i] = 0;
+        this.groupRanks[i] = [];
+      }
 
-    this.rankNums.forEach((groupCount, index) => {
+      this.rankNums.forEach((groupCount, index) => {
         if (groupCount > 0) {
-      ++this.groupCounts[groupCount];
-      this.groupRanks[groupCount].push(index);
-    }});
-    // グループ内, 強い順にソート
-    this.groupRanks.forEach(groupRank => groupRank.sort((a,b) => b - a));
-
-    // ストレート判定
-    let isStraight = false;
-    if (this.groupCounts[1] == HandCardCount) {
-      // 重複するペアがない
-      if (this.hand[HandCardCount - 1] - this.hand[0] === HandCardCount - 4) {
-        // 最大ランクと最少ランクの差が4
-        let isStraight = true;
-      }
-      else if (this.hand[HandCardCount - 1].rank == 1 && this.hand[HandCardCount - 2] == 4){
-        // 最大ランクが1で2番目に大きいランクが4
-        // →A,2,3,4,5 
-        let isStraight = true;
-        // Aが最弱として扱われる
-        this.hand.sort((a,b) => a.rank - b.rank);
-      }
+          ++this.groupCounts[groupCount];
+          this.groupRanks[groupCount].push(index);
+        }});
+      // グループ内, 強い順にソート
+      this.groupRanks.forEach(groupRank => groupRank.sort((a,b) => b - a));
     }
-    this.isStraight = isStraight;
 
+    // ストレート系判定
+    {
+      let isStraight = false;
+      if (this.groupCounts[1] == HandCardCount) {
+        // 重複するペアがない
+        if (this.hand[HandCardCount - 1] - this.hand[0] === HandCardCount - 4) {
+          // 最大ランクと最少ランクの差が4
+          let isStraight = true;
+        }
+        else if (this.hand[HandCardCount - 1].rank == 1 && this.hand[HandCardCount - 2] == 4){
+          // 最大ランクが1で2番目に大きいランクが4
+          // →A,2,3,4,5 
+          let isStraight = true;
+          // Aが最弱として扱われる
+          this.hand.sort((a,b) => a.rank - b.rank);
+        }
+      }
+      this.isStraight = isStraight;
+    }
+
+    // フラッシュ系判定
     // すべてのスートが同じ
     this.isFlush = hand.every(card => card.suit == hand[0].suit);
     this.handRank = __(HandConditions).findLastIndex((e) => e.check(this));
-    this.kickers = __(this.groupRanks.concat().reverse()).flatten();
+    // カードランク評価順
+    // ストレート: 最上位カードを比較
+    // その他: 最も枚数の多いカードグループから順に位を比較
+    this.rankEvaluateOrder = this.isStraight
+      ? this.hand.concat().reverse()
+      : __(this.groupRanks.concat().reverse()).flatten();
   }
 
   // ハンド同士の比較関数
@@ -134,12 +152,13 @@ class HandChecker {
     }
     else {
       // カードの強さで比較
-      if (left.kickers.toString() === right.kickers.toString) {return 0;}
-      else {return left.kickers.findIndex((e, i) => left.kickers < right.kickers[i] >= 0) ? -1 : 1;}
+      if (left.rankEvaluateOrder === right.rankEvaluateOrder) {return 0;}
+      else {return left.rankEvaluateOrder.findIndex((e, i) => left.rankEvaluateOrder < right.rankEvaluateOrder[i] >= 0) ? -1 : 1;}
     } 
   }
 }
 
+// ７枚のカードによる役
 class SevenHand {
   constructor(holds, communities) {
     let cards = communities.concat(holds);
@@ -157,8 +176,8 @@ class SevenHand {
       }
     }
     this.bestHandChecker = bestHandChecker;
-    console.log(HandConditions[this.bestHandChecker.handRank].name);
-    console.log(holds);
+    this.bestHand = bestHandChecker.hand;
+    this.bestHandName = HandConditions[this.bestHandChecker.handRank].name;
   }
 }
 
@@ -172,14 +191,18 @@ class Card {
   pokerRank() {
     return this.rank == 1 ? 14 : this.rank; 
   }
+
+  toString() {
+     return ""+SuitName[this.suit]+RankName[this.rank];
+   }
 }
 
-
-
 class Player {
-  constructor() {
+  constructor(id) {
+    this.id = id;
     this.holds = [];
     this.chip = 3000;
+    this.isWin = false;
   }
 
   checkHand(communities) {
@@ -202,16 +225,28 @@ class Poker {
     for (let i = 0; i < CommunityCardCount; ++i) {
       this.communities.push(this.stack.pop());
     }
-    console.log(this.communities);
     this.players = [];
     for (let i = 0; i < PlayerCount; ++i) {
-      let player = new Player();
+      let player = new Player(i + 1);
       for (let j = 0; j < HoldsCardCount; ++j) {
         player.holds.push(this.stack.pop());
       }
       player.checkHand(this.communities);
       this.players.push(player);
     }
+
+    let bestPlayer = __(this.players.map((player) => ({e: player, comp: player.hand.bestHandChecker}))
+      .sort((a,b) => -HandChecker.compare(a.comp, b.comp)))
+      .first().e;
+
+    console.log(this.communities.map(card => card.toString()).join(' '));
+    console.log();
+    this.players.forEach(player => {
+      console.log("Player"+player.id+":" + (player.id === bestPlayer.id ? ' [WIN]' : ''));
+      console.log(player.holds.map(card => card.toString()).join(' '));
+      console.log(player.hand.bestHandName);
+      console.log();
+    })
   }
 } 
 
